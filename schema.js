47a36568d288a68b2bf974f31a5cc8af2a8327a9
Xin Tao <xtao17@stlawu.edu>
2020-02-20 21:55:43 -0500

    2.20

--- schema.js
+++ schema.js
@@ -9,11 +9,11 @@ const RegistedVoter = new mongoose.Schema({
 });
 
 //Speed up queries on all fields
-Professor.index({n_registered_voter:1});
-Professor.index({first_name:1});
-Professor.index({last_name:1});
-Professor.index({n_2016Election:1});
-Professor.index({n_zipcode:1});
+RegistedVoter.index({n_registered_voter:1});
+RegistedVoter.index({first_name:1});
+RegistedVoter.index({last_name:1});
+RegistedVoter.index({n_2016Election:1});
+RegistedVoter.index({n_zipcode:1});
 //complete and export this Schema
 
 module.exports =mongoose. model('RegistedVoter', RegistedVoter);
