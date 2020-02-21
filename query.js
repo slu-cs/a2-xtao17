a4512b37bffc9ccc3f905adf52db3dca3214f7c9
Xin Tao <xtao17@stlawu.edu>
2020-02-21 11:41:41 -0500

    2.21

--- query.js
+++ query.js
@@ -4,7 +4,7 @@ const RegistedVoter = require('./schema');
 
 connect(); // To the database
 const queries = [
-  RegistedVoter.find({Zip_code:13617})
+  RegistedVoter.find()
 ]
 Promise.all(queries)
   .then(function(results){
