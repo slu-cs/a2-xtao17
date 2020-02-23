93dbc9144fe3c36a5fe90e6baa80ba78eb3bc70c
Xin Tao <xtao17@stlawu.edu>
2020-02-23 16:03:01 -0500

    2.23

--- query.js
+++ query.js
@@ -10,6 +10,5 @@ const queries = [
 Promise.all(queries)
   .then(function(results){
   console.log('Number of voters with zip code 13617:', results[0]);
-
-  console.log('How many zip codes does the county contain?',result[1]);
+  console.log('How many zip codes does the county contain?',results[1]);
   }).catch(error => console.error(error.stack));
