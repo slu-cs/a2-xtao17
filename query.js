80b6f3d9d6635fea60b246c90916c9d885590544
Xin Tao <xtao17@stlawu.edu>
2020-02-23 15:52:35 -0500

    2.23

--- query.js
+++ query.js
@@ -5,12 +5,11 @@ const RegistedVoter = require('./schema');
 connect(); // To the database
 const queries = [
   RegistedVoter.find().where('Zip_code').equals('13617').count(),
-  RegistedVoter.find().where('first_name').equals('STARR'),
   RegistedVoter.distinct('Zip_code')
 ]
 Promise.all(queries)
   .then(function(results){
   console.log('Number of voters with zip code 13617:', results[0]);
-  console.log('full names of all the registered voters whose first-name is STARR:', results[1].map(d=>d.first_name+" "+d.last_name));
+  
   console.log('How many zip codes does the county contain?',result[3]);
   }).catch(error => console.error(error.stack));
