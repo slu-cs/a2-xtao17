62fb777bec2964c40244e275ff36e3fc04321de0
Xin Tao <xtao17@stlawu.edu>
2020-02-23 18:45:43 -0500

    update

--- query.js
+++ query.js
@@ -16,5 +16,6 @@ Promise.all(queries)
   console.log('Number of voters with zip code 13617:', results[0].length);
   console.log('Full Names of all registed voters with first name STARR:',results[1].map(p=>p.first_name+' '+p.last_name));
   console.log('Number of people voted in the 2016 general election:',results[2].length);
-  console.log('How many zip codes does the county contain?',results[3].length);
+  console.log('last-name that comes last in the county in alphabetical order',results[3].map(p=>p.first_name+' '+p.last_name));
+  console.log('How many zip codes does the county contain?',results[4].length);
   }).catch(error => console.error(error.stack));
